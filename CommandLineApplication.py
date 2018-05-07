81575ab9e6594513d339a0530573d8ba77d61f6d
0925648 <svenkalkman1999@gmail.com>
2018-05-07 15:51:27 +0200

    fixed static randint

--- CommandLineApplication.py
+++ CommandLineApplication.py
@@ -1,6 +1,8 @@
 from random import randint
 
-x = randint(0, 9)
+def RandomInteger():
+    return randint(0, 9)
+
 strings1 = ["Het proces ","De factor mens ","Het management ","De communicatie ","De kerncompetentie ","Human capital ","De organisatie-ontwikkeling ","De missie ","Kennismanagement ","De eerste aanzet "]
 strings2 = ["moet meerwaarde leveren bij ","stelt eisen aan ","dient te faciliteren bij ","is uitgangspunt bij ","Is onlosmakelijk verbonden met ","schept voorwaarden voor ","dient te focussen op ","stuurt ","hangt nauw samen met ","moet een opstap bieden voor "]
 strings3 = ["de implementatie van ","de terugkoppeling van ","het aftimmeren van ","het aansturen van ","de ontwikkeling van ","de flexibilisering van ","de integratie van ","de inventarisatie van ","de definitie van ","de insteek van "]
@@ -10,4 +12,4 @@ strings6 = ["waarbij het belang van ","waarbij de feedback van ","waarbij het ka
 strings7 = ["strategisch beleid ","de taskforce ","de communicatie ","de werkgroepen ","new business development ","de systeemintegratie ","de markt ","de stakeholders ","het management ","de projectorganisatie "]
 strings8 = ["moet uitkristalleren.","voorop staat.","wordt aangestuurd.","leading is.","toegevoegde waarde levert.","win-win situaties kan veroorzaken.","moet worden gemanaged.","voldoende draagvlak heeft.","doorslaggevend is.","cruciaal is."]
 
-print(strings1[x] + strings2[x] + strings3[x] + strings4[x] + strings5[x] + strings6[x] + strings7[x] + strings8[x])
+print(strings1[RandomInteger()] + strings2[RandomInteger()] + strings3[RandomInteger()] + strings4[RandomInteger()] + strings5[RandomInteger()] + strings6[RandomInteger()] + strings7[RandomInteger()] + strings8[RandomInteger()])
