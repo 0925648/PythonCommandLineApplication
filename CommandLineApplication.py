a806c404d9e0b8a680d21d0d72037e0b09c4cf88
0925648 <svenkalkman1999@gmail.com>
2018-05-07 15:01:23 +0200

    First commit

---
+++ CommandLineApplication.py
@@ -0,0 +1,13 @@
+from random import randint
+
+x = randint(0, 9)
+strings1 = ["Het proces ","De factor mens ","Het management ","De communicatie ","De kerncompetentie ","Human capital ","De organisatie-ontwikkeling ","De missie ","Kennismanagement ","De eerste aanzet "]
+strings2 = ["moet meerwaarde leveren bij ","stelt eisen aan ","dient te faciliteren bij ","is uitgangspunt bij ","Is onlosmakelijk verbonden met ","schept voorwaarden voor ","dient te focussen op ","stuurt ","hangt nauw samen met ","moet een opstap bieden voor "]
+strings3 = ["de implementatie van ","de terugkoppeling van ","het aftimmeren van ","het aansturen van ","de ontwikkeling van ","de flexibilisering van ","de integratie van ","de inventarisatie van ","de definitie van ","de insteek van "]
+strings4 = ["complexe ","optimale ","in elkaar grijpende ","eenduidige ","onderling afhankelijke ","structurele ","pro-actieve ","resultaatgerichte ","efficiënte ","consistente "]
+strings5 = ["supply chain processen ","business architecture ","mijlpalen ","targets ","business units ","organisatie-onderdelen ","scenario's ","best practices ","business models ","conceptplannen "]
+strings6 = ["waarbij het belang van ","waarbij de feedback van ","waarbij het kader voor ","waarbij afstemming met ","waarbij de structuur van ","waarbij de synergie met ","waarbij de interface met ","waarbij input van ","waarbij commitment van ","waarbij klankborden met "]
+strings7 = ["strategisch beleid ","de taskforce ","de communicatie ","de werkgroepen ","new business development ","de systeemintegratie ","de markt ","de stakeholders ","het management ","de projectorganisatie "]
+strings8 = ["moet uitkristalleren.","voorop staat.","wordt aangestuurd.","leading is.","toegevoegde waarde levert.","win-win situaties kan veroorzaken.","moet worden gemanaged.","voldoende draagvlak heeft.","doorslaggevend is.","cruciaal is."]
+
+print(strings1[x] + strings2[x] + strings3[x] + strings4[x] + strings5[x] + strings6[x] + strings7[x] + strings8[x])
